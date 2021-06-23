67ff5a36818bbf8370dbbe261d7d3c62fd085444
Mário Rabelo <mutrano@gmail.com>
2021-06-22 22:07:55 -0300

    Prometheus rodando e configurado em nosso servico

--- src/main/java/br/com/zupacademy/mario/proposta/WebSecurityConfig.java
+++ src/main/java/br/com/zupacademy/mario/proposta/WebSecurityConfig.java
@@ -11,6 +11,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
+			.antMatchers("/actuator/prometheus").permitAll()
 			.anyRequest()
 			.authenticated()
 			.and()
